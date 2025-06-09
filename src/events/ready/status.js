5e8bb507c41124ca611af18f5f39a0d55fb55099
boolean44-repos <serenitypalmer10@gmail.com>
2025-06-09 13:08:27 -0400

    edited status

--- src/events/ready/status.js
+++ src/events/ready/status.js
@@ -4,7 +4,7 @@ const { url } = require("inspector")
 module.exports = (c, client, handeler) => {
 console.log(`Logged in as ${c.user.tag}`)
 client.user.setActivity({
-    name:(`Helping ${c.guilds.cache.size} servers!`),
+    name:(`Helping ${c.guilds.cache.size} servers`),
     type: ActivityType.Streaming,
     url:("https://www.youtube.com/watch?v=dQw4w9WgXcQ") 
 })
