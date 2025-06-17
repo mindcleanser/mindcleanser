4e5bbb7f40a7355c271891ab3910ec7c44e48450
boolean44-repos <serenitypalmer10@gmail.com>
2025-06-16 21:11:35 -0400

    test

--- src/events/ready/status.js
+++ src/events/ready/status.js
@@ -4,7 +4,7 @@ const { url } = require("inspector")
 module.exports = (c, client, handeler) => {
 console.log(`Logged in as ${c.user.tag}`)
 client.user.setActivity({
-    name:(`Helping ${c.guilds.cache.size} servers`),
+    name:(`Helping ${c.guilds.cache.size} servers!`),
     type: ActivityType.Streaming,
     url:("https://www.twitch.tv/mindcleanser/about") 
 })
